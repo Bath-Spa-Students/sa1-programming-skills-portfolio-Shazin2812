4276017ad20b432515271ffe593cf76ce3ca2a39
Shazin <shazinhussain2812@gmail.com>
2024-11-07 20:16:42 +0400

    Some Counting.py

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,39 @@
+# Exercise 7: Some Counting
+# The Question.
+"""Use your newly acquired knowledge of the for loop to complete the following
+tasks. Print all values to the console in each case. * Write a loop that counts
+up from 0 to 50 in increments of 1. * Write a loop that counts down from 50 to
+0 in decrements of 1. * Write a loop that counts up from 30 to 50 in increments
+of 1. * Write a loop that counts down from 50 to 10 in decrements of 2. * Write
+a loop that counts up from 100 to 200 in increments of 5. You may include all
+loops in a single project"""
+# Output.
+# Write a loop that counts up from 0 to 50 in increments of 1.
+print(" Outputing numbers up from 0 to 50: ")
+# using the for in loop function to print 0 to 50 in increments of 1.
+for z in range(0, 51, 1):
+    print (z)
+
+# Write a loop that counts down from 50 to 0 in decrements of 1.
+print(" Outputing numbers down from 50 to 0: ")
+# using the for in loop function to print 50 to 0 in decrements of 1.
+for y in range(50, -1, -1):
+    print (y)
+
+# Write a loop that counts up from 30 to 50 in increments of 1.
+print(" Outputing numbers up from 30 to 50: ")
+# using the for in loop function to print 30 to 50 in increments of 1.
+for x in range(30, 51, 1):
+    print (x)
+
+# Write a loop that counts down from 50 to 10 in decrements of 2.
+print(" Outputing numbers down from 50 to 10 in decrements of 2: ")
+# using the for in loop function to print 50 to 10 in decrements of 2.
+for w in range(50, 9, -2):
+    print (w)
+
+#  Write a loop that counts up from 100 to 200 in increments of 5.
+print(" Outputing numbers up from 100 to 200 in increments of 5: ")
+# using the for in loop function to print 100 to 200 in increments of 5.
+for v in range(100, 201, 5):
+    print (v)
