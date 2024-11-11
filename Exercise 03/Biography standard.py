b5289039ea88bee2928d26c6df20e60952828fdc
Shazin <shazinhussain2812@gmail.com>
2024-11-11 12:22:44 +0400

    Biography standard.py

---
+++ Exercise 03/Biography standard.py
@@ -0,0 +1,17 @@
+# Exercise 3: Biography standard
+"""In this exercise, you’ll create a program that stores and prints your name, home-
+town, and age to the console using a Python dictionary."""
+#Steps:
+"""1. Store the information (name, hometown, and age) as key-value pairs in a
+dictionary.
+2. Print the values on separate lines using a single print() statement.
+3. Use variables with appropriate data types for each piece of information."""
+
+# Storing information as key-value pairs in a dictionary
+person_information = {
+    "name": "Shazin",
+    "hometown": "Sharjah",
+    "age": 18}
+
+# Checking if the values on separate lines in a single print statement
+print(f"Name: {person_information['name']}\nHometown: {person_information['hometown']}\nAge: {person_information['age']}")
