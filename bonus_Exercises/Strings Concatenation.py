80f0e453475dc81bc175819549842840466a722c
Shazin <shazinhussain2812@gmail.com>
2024-11-18 19:03:27 +0400

    Strings Concatenation.py

---
+++ bonus_Exercises/Strings Concatenation.py
@@ -0,0 +1,10 @@
+# Strings Concatenation:
+"""Write three strings in different variables and print the output as one string."""
+# Output
+# Writing three strings in different variables
+Str1 = "2024"
+Str2 = "SEASON COMES TO"
+Str3 = "AN END...."
+# Making all 3 strings and print as one string 
+GOODBYE = Str1 + " " + Str2 + " " + Str3
+print(GOODBYE)
