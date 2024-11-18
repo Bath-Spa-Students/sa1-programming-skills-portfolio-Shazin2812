617f814a993305c22420066680c78a852437ea5d
Shazin <shazinhussain2812@gmail.com>
2024-11-18 19:07:49 +0400

    Dictionaries.py

---
+++ bonus_Exercises/Dictionaries.py
@@ -0,0 +1,18 @@
+# Dictionaries:
+"""Use a dictionary to store information about a person you know.Store their first name,
+last name, age, and the city in which they live.
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary."""
+# Output
+# Creating a dictionary to store personal information
+personal_information = {
+    "first_name": "Shazin",
+    "last_name": "Hussain",
+    "age": 18,
+    "city": "SHJ"
+}
+# Printing each things which stored in the personal information
+print("First Name:", personal_information["first_name"])
+print("Last Name:", personal_information["last_name"])
+print("Age:", personal_information["age"])
+print("City:", personal_information["city"])
