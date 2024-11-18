cd3e60028fdb58df53ea4db4d7ee78be246636e4
Shazin <shazinhussain2812@gmail.com>
2024-11-18 19:05:42 +0400

    Variables (Favorite Number).py

---
+++ bonus_Exercises/Variables (Favorite Number).py
@@ -0,0 +1,10 @@
+# Variables (Favorite Number):
+"""Use a variable to represent your favorite number. Then, using that variable, create a
+message that reveals your favorite number. Print that message."""
+#Output
+# Writing my favorite number
+Favorite_Num = 28
+# Creating a message writing my favorite number
+message = f"My favorite Number is {Favorite_Num}."
+# And printing it
+print(message)
