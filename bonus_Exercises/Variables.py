5340d568da7369445e829de026e0561e6358e228
Shazin <shazinhussain2812@gmail.com>
2024-11-18 19:05:03 +0400

    Variables.py

---
+++ bonus_Exercises/Variables.py
@@ -0,0 +1,10 @@
+# Variables:
+"""Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message."""
+# Output
+# Writing a word to a variable
+Msg= "Byee, 2024..."
+print(Msg)
+# Changing the word of the variable to a new word
+Msg = "Welcome to 2025!"
+print(Msg)
