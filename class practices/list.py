0ab0880de58e1a140fa1750c49257d4941b9e54a
Shazin <shazinhussain2812@gmail.com>
2024-11-19 13:38:32 +0400

    list.py

---
+++ class practices/list.py
@@ -0,0 +1,19 @@
+# Postive indexing
+num=[100,99,98,97,96,95]
+print(num[4])
+
+# Negative indexing
+num=[-94,-93,-92,-91,-90]
+print(num[-1])
+
+# Finding length of list
+num=[89,88,87,86,85]
+print("The Num of Elements=",len(num))
+
+# Finding index
+num=[84,83,82,81,80]
+print(num.index(81))
+
+# Slicing
+num=[79,78,77,76,75]
+print(num.sort())
