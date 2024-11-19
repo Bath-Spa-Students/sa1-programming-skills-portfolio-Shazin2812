bee2f618e423e30f1f79862f9a1a9d176b3f0fff
Shazin <shazinhussain2812@gmail.com>
2024-11-19 13:36:25 +0400

    End.py

---
+++ class practices/End.py
@@ -0,0 +1,8 @@
+# End delimiter will print the statements on the same lines
+Name="Shazin Hussin"
+print(Name,End=' ') 
+print("Printing a New Line Message")
+
+#/n and /t are known as escape sequences
+print("\n this line will print on the next line")
+print("\t this is tabbed in")
