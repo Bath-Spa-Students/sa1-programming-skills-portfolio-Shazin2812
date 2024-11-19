6d5d6cbf15ff2bb1d44d04839ef615145019926d
Shazin <shazinhussain2812@gmail.com>
2024-11-19 13:51:44 +0400

    String concatenation.py

---
+++ class practices/String concatenation.py
@@ -0,0 +1,33 @@
+# String concatenation through given variable
+msg='Your '+'the '+'Best '+'thing '+'in'+'the'+'world'
+print(msg)
+
+# String concat through given print statement
+print("Bathspa University "+"UK"+"Academic centre "+"RAK campus")
+
+# String concat through different variable
+a='United'
+b='Arab'
+c='Emirates'
+d=a+" "+b+" "+c
+print(d)
+
+# String concatenationi through given variable
+msg='SHAZIN '+ 'HUSSAIN'
+print(msg)
+
+# String concatenationi through print statement
+print("SHAZIN "+"HUSSSAIN")
+
+# String concatenationi through different variable
+a='SHAZIN'
+b='HUSSAIN'
+c=a+" "+b
+print(c)
+
+# String concat through different variable
+a='Today'
+b='is'
+c='Friday'
+d=a+" "+b+" "+c
+print(d)
