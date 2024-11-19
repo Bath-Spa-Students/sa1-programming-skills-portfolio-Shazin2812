87a0c34aef82e908ab2d492513fd961188630a2c
Shazin <shazinhussain2812@gmail.com>
2024-11-19 13:36:56 +0400

    function.py

---
+++ class practices/function.py
@@ -0,0 +1,18 @@
+# Function
+# Diffenrent functions have same local variables name - No syntax error
+ 
+# Function header
+def print_message():
+# Local variable
+  message ="Shazin" 
+  print(message)
+
+# Function header
+def print_message_2():
+# Changing the Local variable
+  message ="Chachu"
+  print(message)
+
+# Function call
+print_message()
+print_message_2() 
