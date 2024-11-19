ab457e399f35953e98e831dfa0f6ec0b16b08330
Shazin <shazinhussain2812@gmail.com>
2024-11-19 13:38:03 +0400

    input.py

---
+++ class practices/input.py
@@ -0,0 +1,15 @@
+# Asking the user to input
+First_Name=input("Enter your Name: ")
+print(First_Name)
+
+Middle_Name=input("Enter your Middle Name: ")
+print(Middle_Name)
+
+Last_Name=input("Enter your Last Name: ")
+print(Last_Name)
+
+Age=int(input("Enter your Age: "))
+print(Age)
+
+Place=input("Enter your Place: ")
+print(Place)
