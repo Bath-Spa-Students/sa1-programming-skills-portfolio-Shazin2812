684f81a7e48323f16f5d64425501bca70e8a3797
Shazin <shazinhussain2812@gmail.com>
2024-11-21 18:33:26 +0400

    Turtles.py

---
+++ class practices/Turtles.py
@@ -0,0 +1,46 @@
+# Making a many circle using a turtle
+import turtle
+turtle.bgcolor("black")
+turtle.pensize(7)
+turtle.pencolor('red')
+turtle.circle(100)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('blue')
+turtle.circle(90)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('green')
+turtle.circle(80)
+import turtle
+turtle.pensize(5)
+turtle.pencolor('yellow')
+turtle.circle(70)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('orange')
+turtle.circle(60)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('white')
+turtle.circle(50)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('grey')
+turtle.circle(40)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('pink')
+turtle.circle(30)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('purple')
+turtle.circle(20)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('brown')
+turtle.circle(10)
+import turtle
+turtle.pensize(7)
+turtle.pencolor('teal')
+turtle.circle(0)
