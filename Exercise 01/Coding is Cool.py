b27be4c96453b009860f251bdd364b40d311057c
Shazin <shazinhussain2812@gmail.com>
2024-11-21 18:35:22 +0400

    Coding is Cool.py

--- Exercise 01/Coding is Cool.py
+++ Exercise 01/Coding is Cool.py
@@ -1,4 +1,12 @@
+#Exercise 1: Coding is Cool
+"""Fill in the blanks in the Python code below to output the phrase “Coding is
+Cool” to the console using variables and string concatenation."""
+"""Fill in the blanks below
+word1 =
+word2 =
+word3 =                          """
 #Fill in the concatenation variables 
+#Answer
 word1 = "Coding "
 word2 = "is " 
 word3 = "cool"
